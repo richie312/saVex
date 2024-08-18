07920893d5f1d24656f977575d672a9e07e6758e
richie312 <richie.chatterjee31@gmail.com>
2024-08-18 19:26:16 +0530

    mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -99,13 +99,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ContainerDatabase',
+        'USER': 'root',
+        'PASSWORD': 'Nirvikalpa#123',
+        'HOST': '192.168.1.20',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
