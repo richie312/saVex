574da54fedc502f3e4cffbeab7070f7ed5943b56
richie312 <richie.chatterjee31@gmail.com>
2024-10-03 11:51:59 +0530

    migration commands in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# Run Migration command
+python manage.py makemigrations
+
+# Migrate
+python manage.py migrate
+
 # Run your Django management command
 while true; do python manage.py command 10000; sleep 60 * 60 * 12; done
 
 # Start your Django project
-python manage.py runserver 0.0.0.0:8000
+python manage.py dev runserver 0.0.0.0:8000
